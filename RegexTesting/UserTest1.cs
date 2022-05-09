c8745888f667a270d5e308a23a3879e59c1232b7
Rahmath-Unnisa <asfiarehmath@gmail.com>
2022-05-09 22:49:19 +0530

    [Rahmath]Add: User Email Name Validation

--- RegexTesting/UserTest1.cs
+++ RegexTesting/UserTest1.cs
@@ -3,16 +3,16 @@ using ValidatingRegex;
 
 namespace RegexTesting
 {
-        public class UserTest1
-        {
+    public class UserTest1
+    {
 
-            [Test]
-            public void FirstName()
-            {
-                UserValidation validation = new UserValidation();
-                string actual = validation.FirstName("Manha");
-                Assert.AreEqual(actual, "Manha");
-            }
+        [Test]
+        public void FirstName()
+        {
+            UserValidation validation = new UserValidation();
+            string actual = validation.FirstName("Manha");
+            Assert.AreEqual(actual, "Manha");
+        }
         [Test]
         public void LastName()
         {
@@ -20,7 +20,14 @@ namespace RegexTesting
             string actual = validation.LastName("Mehwish");
             Assert.AreEqual(actual, "Mehwish");
         }
-
+        [Test]
+        public void Email()
+        {
+            UserValidation validation = new UserValidation();
+            string actual = validation.EmailValidation("asfiarehmath@gmail.com");
+            Assert.AreEqual(actual, "asfiarehmath@gmail.com");
+        }
     }
+    
     
 }
--- ValidatingRegex/UserValidation.cs
+++ ValidatingRegex/UserValidation.cs
@@ -6,6 +6,7 @@ namespace ValidatingRegex
     {
         public const string FIRSTNAME_REGEX = "^[A-Z]{1}[a-zA-Z]{2,}$";
         public const string LASTNAME_REGEX = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        public static string EMAIL_REGEX = "^[0-9a-zA-Z]+[.+-_]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
         public string FirstName(string firstname)
         {
             if (Regex.IsMatch(firstname, FIRSTNAME_REGEX))
@@ -29,6 +30,18 @@ namespace ValidatingRegex
                 Console.WriteLine("Verify Last Name Again");
             }
             return lastname;
+        }
+        public string EmailValidation(string email)
+        {
+            if (Regex.IsMatch(email, EMAIL_REGEX))
+            {
+                Console.WriteLine("Email Name Matches");
+            }
+            else
+            {
+                Console.WriteLine("Verify Email Again");
+            }
+            return email;
         }
     }
 }
